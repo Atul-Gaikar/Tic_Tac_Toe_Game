f0ff8fa67f3470dde7bbf7452511f5c3d80549b5
Atul <atulgaikarag9@gmail.com>
2021-06-30 21:11:32 +0530

    UC1 User friendly

---
+++ src/com/bridgelabz/TicTacToeGame/TicTacToeGame.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.TicTacToeGame;
+
+public class TicTacToeGame {
+	public static void main(String args[]) {
+		System.out.println("Welcome to TicTacToe Game");
+		TicTacToeGame p = new TicTacToeGame(); // Create a object.
+		p.createBoard();
+
+	}
+
+	public void createBoard() {
+		char[] board = new char[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };// Create a board of char[]of size 10 & assign empty
+																	// space to each element
+
+	}
+
+}
