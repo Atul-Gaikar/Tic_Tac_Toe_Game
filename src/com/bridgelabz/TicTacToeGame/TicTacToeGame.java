1191ee0aec1c94d2f17b3925819a59f0b3530e2d
Atul-Gaikar <84188234+Atul-Gaikar@users.noreply.github.com>
2021-07-01 09:02:00 +0530

    Update TicTacToeGame.java

--- src/com/bridgelabz/TicTacToeGame/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame/TicTacToeGame.java
@@ -1,23 +1,17 @@
-package com.bridgelabz;
+package com.bridgelabz.TicTacToeGame;
 
-import java.util.Arrays;
-import java.util.Scanner;
+public class TicTacToeGameUC1 {
+	public static void main(String args[]) {
+		System.out.println("Welcome to TicTacToe Game");
+		TicTacToeGameUC1 p = new TicTacToeGameUC1(); // Create a object.
+		p.createBoard();
 
-public class TicTacToeGame {
-	private static final char empty = ' ';
-	private char[] board;
-
-		TicTacToeGame(){
-		board = new char[10];
-		for(int i = 0;i<board.length; i++) {
-			board[i] = empty;
-		}
 	}
 
-	public static void main(String[] args) {
-		TicTacToeGame game = new TicTacToeGame();
-		game.TicTacToeGame();
+	public void createBoard() {
+		char[] board = new char[] { 10 };// Create a board of char[]of size 10 & assign empty
+																	// space to each element
+
 	}
-}
 
 }
